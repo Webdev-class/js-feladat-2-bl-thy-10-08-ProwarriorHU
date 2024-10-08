d41935600ca57c8bd3fb37796eb7eecca045d090
ShepherdMate <juhasz.mate.laszlo@blathy.info>
2024-10-08 18:50:45 +0200

    megoldás
    nem működik de nem tudtam rá jönni miért

--- f1/f1.js
+++ f1/f1.js
@@ -1,7 +1,11 @@
 
 const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
 async function f1 (lat, long, timeStr) {
-    //TODO
+  let tempavg = await fetch(APIAdress + `latitude=${lat}&longitude=${long}` +
+    `&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`)
+  .then(data => data.json())
+  .then(data => data.hourly.temperature_2m)
+  return Math.floor((tempavg.reduce((x, y) => x + y)) / (tempavg.length));
 }
 
 module.exports = f1;
